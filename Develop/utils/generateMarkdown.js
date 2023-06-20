186abdce77ab7a46f58b64e4cd5b54cd3713818b
Sarah Squyres <ssquyres15@gmail.com>
2023-06-20 18:36:45 -0500

    still trying to fix sample readme

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -17,36 +17,36 @@ function generateMarkdown(data) {
 * [How to Contribute](#how-to-contribute)
 * [Testing](#tests)
 * [Questions](#questions)
-<br>
+
 ## Description
 ${data.description}
-<br>
+
 ## Installation
 ${data.installation}
-<br>
+
 ## Usage
 ${data.usage}
-<br>
+
 ## License
 Licensed under the ${data.license} license.
-<br>
+
 ## How to Contribute
 ${data.contributing}
-<br>
+
 ## Testing
 ${data.test}
-<br>
+
 ## Questions
 ${data.questions}
-<br>
+
 Please feel free to email me at: 
-<br>
+
 ${data.email} 
 
 Check out my GitHub repos!
-<br>
+
 Here's the link to my GitHub:
-<br>
+
  [${data.github}](https://github.com/${data.github}/)
 `;
 }
